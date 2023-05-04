9fddd0169f0ce16dc690fda80f5c4937e5040fd5
Kamrul101 <ridoyhasan87@gmail.com>
2023-05-04 22:51:51 +0600

    env

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBETb33YD4q518HtFDx2U_OFeSCczSFz64",
-  authDomain: "recipe-hunter-client.firebaseapp.com",
-  projectId: "recipe-hunter-client",
-  storageBucket: "recipe-hunter-client.appspot.com",
-  messagingSenderId: "859545605824",
-  appId: "1:859545605824:web:ffa5db67d109b8d78fff25"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID
 };
 
 // Initialize Firebase
